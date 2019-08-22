9a4d869ae3806aa2b23a0951011a8a259be8a24c
Abdulla Bubshait <darkstego@gmail.com>
2019-08-22 16:35:07 +0300

    Added visual-fill-column

--- init-user.el
+++ init-user.el
@@ -6,6 +6,9 @@
 ;; Auto-save org-mode buffers
 (add-hook 'auto-save-hook 'org-save-all-org-buffers)
 
+(use-package visual-fill-column
+  :config
+  (add-hook 'visual-line-mode-hook #'visual-fill-column-mode))
 
 
 (provide 'init-user)
