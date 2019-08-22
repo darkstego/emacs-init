bada3f403cfb2dc5f96deaed4ff4b7770fdef934
Abdulla Bubshait <darkstego@gmail.com>
2019-08-22 16:25:30 +0300

    First commit

---
+++ init-user.el
@@ -0,0 +1,11 @@
+;; Place all local configuration options here
+
+;; Automatically create closing parenthesis/quote
+(electric-pair-mode 1)
+
+;; Auto-save org-mode buffers
+(add-hook 'auto-save-hook 'org-save-all-org-buffers)
+
+
+
+(provide 'init-user)
